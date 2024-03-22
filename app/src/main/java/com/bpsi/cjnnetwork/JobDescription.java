5326b03da67148e121934af08ec336e66de33ae4
VishalG2001 <vishalguptalx@gmail.com>
2024-03-22 23:10:47 +0530

    added jobdescription activity

---
+++ app/src/main/java/com/bpsi/cjnnetwork/JobDescription.java
@@ -0,0 +1,14 @@
+package com.bpsi.cjnnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_description);
+    }
+}
